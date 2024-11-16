f5cc2830ecb0090f1c6ad95517923881db873f22
SriAlanyaReddy <srialanyareddy@gmail.com>
2024-11-16 15:33:55 +0530

    sample maven java project

---
+++ src/main/java/com/app1/demo/sample_maven_java_project/App.java
@@ -0,0 +1,13 @@
+package com.app1.demo.sample_maven_java_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "22BD1A6629 SRI ALANYA" );
+    }
+}
